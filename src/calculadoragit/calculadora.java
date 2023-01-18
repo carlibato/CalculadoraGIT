e2f5ec78d95dab70af83ed93037f4d1f4a24b0f5
“carlibato” <carlibato@gmail.com>
2023-01-18 09:35:12 +0100

    Proyecto Base

---
+++ src/calculadoragit/calculadora.java
@@ -0,0 +1,5 @@
+package calculadoragit;
+
+public class calculadora {
+
+}
---
+++ src/calculadoragit/principal.java
@@ -0,0 +1,10 @@
+package calculadoragit;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
